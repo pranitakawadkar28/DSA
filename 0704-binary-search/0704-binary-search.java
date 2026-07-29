c39ea637374d8cd7896bd170b5c20a332764c5f6
Pranita <pranitakawadkar10@gmail.com>
2026-07-30 00:58:05 +0530

    Time: 0 ms (100%), Space: 48.2 MB (69.6%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int s = 0;
+        int l = nums.length - 1;
+
+        while (s <= l) {
+
+            int mid = s + (l - s) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            else if (nums[mid] < target) {
+                s = mid + 1;
+            } 
+            else {
+                l = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
