b177101eee99d63b7c5ed98bc1da427ad17d565a
Pranita <pranitakawadkar10@gmail.com>
2026-08-06 10:51:19 +0530

    Time: 5 ms (81.12%), Space: 46.3 MB (6.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        
-        if(x < 0) {
+        if(x < 0){
             return false;
         }
 
         int sum = 0;
         int target = x;
- 
-        while(x > 0){
+
+        while(x != 0){
             int temp = x % 10;
             x = x / 10;
             sum = sum * 10 + temp;
