2098d6833c055bf807f19290d9f60d10e87cf186
Pranita <pranitakawadkar10@gmail.com>
2026-08-06 11:05:49 +0530

    Time: 2 ms (99.32%), Space: 44.5 MB (55.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,34 +3,27 @@ class Solution {
         int left = 0;
         int right = s.length() - 1;
 
-        while (left < right) {
-
+        while(left < right){
             char l = s.charAt(left);
             char r = s.charAt(right);
 
-            if (!isAlphaNum(l)) {
-                left++;
-            }
-
-            else if (!isAlphaNum(r)) {
-                right--;
-            }
+            if(!isAlphaNum(l)) left++;
+            else if(!isAlphaNum(r)) right--;
 
             else {
-
-                if (Character.toLowerCase(l) != Character.toLowerCase(r))
-                    return false;
+                if(Character.toLowerCase(l) !=
+                   Character.toLowerCase(r)
+                ) return false;
 
                 left++;
                 right--;
             }
         }
-
         return true;
-    }
+     }
 
     private boolean isAlphaNum(char c){
-        return 
+        return
         (c >= 'a' && c <= 'z') ||
         (c >= 'A' && c <= 'Z') ||
         (c >= '0' && c <= '9');
